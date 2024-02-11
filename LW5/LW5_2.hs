19b1ab6efa792533581b3e2a00f4cf2429396d91
Artem-Cherkasov <robbit31@yandex.ru>
2024-02-11 16:55:08 +0300

    LW5_2

---
+++ LW5/LW5_2.hs
@@ -0,0 +1,22 @@
+import Data.Char
+import System.Environment
+import System.IO
+
+prompt :: String -> IO String
+prompt text = do
+    putStr text
+    hFlush stdout
+    getLine
+
+main :: IO ()
+main = do
+
+  [f1,f2] <- getArgs
+
+  content <- readFile f1
+
+  replaceChar <- prompt "Enter replacer character: "
+
+  let replaced = map (\c -> if isPunctuation c then head replaceChar else c) content
+
+  writeFile f2 replaced
